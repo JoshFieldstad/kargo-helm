4059f0678794e42a70d8daf43c8f85f9ea7961be
jfieldstad <josh.fieldstad@gmail.com>
2025-04-04 17:05:28 -0600

    feat: version bump

--- buildDocker.sh
+++ buildDocker.sh
@@ -3,11 +3,5 @@ docker login ghcr.io
 
 docker buildx imagetools create \
   ghcr.io/akuity/guestbook:v0.0.1 \
-  -t ghcr.io/joshfieldstad/guestbook:v0.0.1
+  -t ghcr.io/joshfieldstad/guestbook:v0.0.3
 
-
-  kargo create credentials github-creds \
-  --project kargo-helm \
-  --git \
-  --username JoshFieldstad \
-  --repo-url https://github.com/joshfieldstad/kargo-helm
